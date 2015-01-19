5c608a9ae4497e421db64c4d8153da2acddb84a2
Emanuel Langit <emanuel.langit@fastmail.fm>
2015-01-19 00:37:26 -0800

    Started GSWL4 book
    Left of at page 36

---
+++ cats/app/views/about.php
@@ -0,0 +1,7 @@
+<!--
+The most frequent object that you will return from your routes is the View object.
+Views receive data from a route (or controller) and inject it into a template, therefore,
+helping you to separate the business and presentation logic in your application. (p.35)
+-->
+<h2>About this site</h2>
+There are over <?php echo $number_of_cats; ?> cats on this site!
