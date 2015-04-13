c96d48b30e9929cb4f87a4613a392374ac7b2fb1
Emanuel Langit <emanuel.langit@fastmail.fm>
2015-04-12 17:14:07 -0700

    Changed home page
    Removed default home view (home.jsp) and HomeController.java. Made the
    sequenceInput form page the home page.

--- SpringMvcImplementation/src/main/java/com/emanuellangit/dissertation/controller/SequenceInformationController.java
+++ SpringMvcImplementation/src/main/java/com/emanuellangit/dissertation/controller/SequenceInformationController.java
@@ -2,7 +2,6 @@ package com.emanuellangit.dissertation.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.emanuellangit.dissertation.model.SequenceInformation;
 
+
+
 @Controller
-@RequestMapping(value="/sequenceInput")
+@RequestMapping(value="/") // partial address (this comes after the home URL)
 public class SequenceInformationController {
 	
 	@RequestMapping(method=RequestMethod.GET)
@@ -20,7 +21,7 @@ public class SequenceInformationController {
 		//model.addAttribute("sequenceInformation", new SequenceInformation());
 		
 		model.addAttribute("sequenceInformation", new SequenceInformation());
-		return "sequenceInputFormPage";
+		return "sequenceInputFormPage"; // filename of view (.jsp file)
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
@@ -28,7 +29,7 @@ public class SequenceInformationController {
 		
 		model.addAttribute("sequenceInformation", sequenceInformation);
 		
-		return "sequenceSubmittedPage";
+		return "sequenceSubmittedPage"; // filename of view (.jsp file)
 	}
 	
 	/*
